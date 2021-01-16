b13c2a20a3f1975b46c70d2e5b4d8f03b517ea1b
Cyril Saidane <cs.saidane@gmail.com>
2021-01-16 13:29:58 -0500

    bug discovered in logo display

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,14 +4,8 @@ import Movie from "./Movie";
 export default function Results(props) {
   const {results} = props;
 
-  if(results.Search){
-    
-      return results.Search
-        .map(movie => {
-          return <Movie key={movie.imdbID} {...movie} nominations={props.nominations} setNominations={props.setNominations}/>;
-        });
-
-
-
-  }
+  return results.Search
+    .map(movie => {
+      return <Movie key={movie.imdbID} {...movie} nominations={props.nominations} setNominations={props.setNominations}/>;
+    });
 }
